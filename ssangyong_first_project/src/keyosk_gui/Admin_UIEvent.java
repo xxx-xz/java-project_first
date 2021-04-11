51cc1c8da79b678cde409d370f4d22a16365ca54
Limhseop <74372615+Limhseop@users.noreply.github.com>
2021-04-12 00:17:02 +0900

    admin
    admin

---
+++ ssangyong_first_project/src/keyosk_gui/Admin_UIEvent.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+public class Admin_UIEvent implements ActionListener {
+	login main;
+	
+	
+	
+	public Admin_UIEvent(login main) {
+		this.main = main;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object obj = e.getSource();
+		if(obj == main.join_bt) {
+			main.layeredPane.dispose();
+			main.layeredPane.setVisible(false);
+			new join();
+			
+			
+		}
+		
+		
+
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
