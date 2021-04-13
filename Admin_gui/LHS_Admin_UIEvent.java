750ad2442e7beb3b524a43633a8be674300d99e2
Limhseop <74372615+Limhseop@users.noreply.github.com>
2021-04-14 01:07:41 +0900

    Add files via upload

---
+++ Admin_gui/LHS_Admin_UIEvent.java
@@ -0,0 +1,35 @@
+package project_gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LHS_Admin_UIEvent implements ActionListener {
+	LHS_Admin_Login main;
+	
+	
+	
+	public LHS_Admin_UIEvent(LHS_Admin_Login main) {
+		this.main = main;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object obj = e.getSource();
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
