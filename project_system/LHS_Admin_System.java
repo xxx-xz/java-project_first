65a3f63b48f0c51815c410810c3c31ec9c915bd8
Limhseop <74372615+Limhseop@users.noreply.github.com>
2021-04-26 18:10:50 +0900

    admin

--- project_system/LHS_Admin_System.java
+++ project_system/LHS_Admin_System.java
@@ -23,4 +23,12 @@ public class LHS_Admin_System {
 	public boolean join(LHS_Admin_VO member) {
 		return mdao.getJoinResult(member);
 	}
+	
+	public boolean search(String id, String email) {
+		return mdao.search(id, email);
+	}
+	public int update(String id, String pass, String pass2) {
+		System.out.println("system");
+		return mdao.update(id, pass, pass2);
+	}
 }
